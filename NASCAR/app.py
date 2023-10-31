9f88e155e436c02e4fd251168de01d539a124e60
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-10-31 10:20:37 +0100

    redo app.py+add get_info_driver_manufacturer

--- NASCAR/app.py
+++ NASCAR/app.py
@@ -1,43 +1,93 @@
+# import sys
+# import os
+# from getinfo import *
+#
+# restart = "N"
+#
+# def restart_program():
+#     python = sys.executable
+#     os.execl(python, python, *sys.argv)
+#
+#
+# def commands():
+#     print("for help do /help or /?")
+#     print("to close the app /exit ")
+#
+#     command = input("Type Command here: ")
+#
+#     if command == "/standings" or command == "/standings_name" or command == "/standings_best" or command == "/exit" or command == "/help" or command == "/?" or command == '/standings_manufacturer':
+#         if command == "/standings":
+#             get_info()
+#             commands()
+#         elif command == "/exit":
+#             print("program Offline")
+#         elif command == "/standings_name":
+#             searchname = input("Input driver name here: ")
+#             get_info_from_name(searchname)
+#             commands()
+#         elif command == "/standings_best":
+#             get_info_best()
+#             commands()
+#         elif command == "/standings_manufacturer":
+#             searchname = input("Input driver name here: ")
+#             get_info_manufacturer()
+#             commands()
+#         elif command == "/help" or command == "/?":
+#             print("to get the current standings Use the command /standings ")
+#             print("to get the current standings filtered by name Use the command /standings_name ")
+#             print("to get the current standings with highest points Use the command /standings_best ")
+#             print("to get the current standings filtered by manufacturer Use the command /standings_manufacturer ")
+#             commands()
+#     else:
+#         print("Command not found")
+#         commands()
+#
+# print("Welcome to the Unofficial NASCAR info Python script")
+# commands()
+
 import sys
 import os
-from getinfo import get_info
-from getinfo import get_info_from_name
-from getinfo import get_info_best
-
+from getinfo import *
 restart = "N"
-
 def restart_program():
     python = sys.executable
     os.execl(python, python, *sys.argv)
-
-
+def get_standings():
+    get_info()
+def get_standings_name():
+    searchname = input("Input driver name here: ")
+    get_info_from_name(searchname)
+def get_standings_best():
+    get_info_best()
+def get_standings_manufacturer():
+    get_info_driver_manufacturer()
+def display_help():
+    print("to get the current standings Use the command /standings ")
+    print("to get the current standings filtered by name Use the command /standings_name ")
+    print("to get the current standings with highest points Use the command /standings_best ")
+    print("to get the current standings with highest points Use the command /standings_manufacturer ")
+def exit_program():
+    print("Program Offline")
+    sys.exit()
+commands_dict = {
+    "/standings": get_standings,
+    "/standings_name": get_standings_name,
+    "/standings_best": get_standings_best,
+    "/standings_manufacturer": get_standings_manufacturer,
+    "/help": display_help,
+    "/?": display_help,
+    "/exit": exit_program
+}
 def commands():
     print("for help do /help or /?")
     print("to close the app /exit ")
-
     command = input("Type Command here: ")
-
-    if command == "/standings" or command == "/standings_name" or command == "/standings_best" or command == "/exit" or command == "/help" or command == "/?":
-        if command == "/standings":
-            get_info()
-            commands()
-        elif command == "/exit":
-            print("program Offline")
-        elif command == "/standings_name":
-            searchname = input("Input driver name here: ")
-            get_info_from_name(searchname)
-            commands()
-        elif command == "/standings_best":
-            get_info_best()
-            commands()
-        elif command == "/help" or command == "/?":
-            print("to get the current standings Use the command /standings ")
-            print("to get the current standings filtered by name Use the command /standings_name ")
-            print("to get the current standings with highest points Use the command /standings_best ")
-            commands()
+    command_function = commands_dict.get(command, None)
+    if command_function:
+        command_function()
+        commands()
     else:
         print("Command not found")
         commands()
-
 print("Welcome to the Unofficial NASCAR info Python script")
 commands()
--- NASCAR/getinfo.py
+++ NASCAR/getinfo.py
@@ -2,7 +2,7 @@ from sql_client import SQLClient
 sql_client = SQLClient()
 
 def get_info():
-    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #` FROM standings;")
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #`, Points FROM standings;")
 
     current_standings_str = str(current_standings)
 
@@ -12,7 +12,7 @@ def get_info():
 
 def get_info_from_name(name):
     name = str(name)
-    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #` FROM standings WHERE Driver = '" + name + "';")
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Driver = '" + name + "'';")
 
     current_standings_str = str(current_standings)
 
@@ -22,6 +22,18 @@ def get_info_from_name(name):
 
 def get_info_best():
     current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE `Points` = (SELECT MAX(`Points`) FROM standings);")
+
+    current_standings_str = str(current_standings)
+
+    print("The current NASCAR Cup Standings are: ")
+    for data in current_standings:
+        print(data)
+
+def get_info_driver_manufacturer():
+    manu = str(sql_client.fetch_all("SELECT DISTINCT Make FROM standings"))
+    print('The Manufacturers are' + manu)
+    name = str(input("Input Manufacturer name here: "))
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Make = '" + name + "';")
 
     current_standings_str = str(current_standings)
 
