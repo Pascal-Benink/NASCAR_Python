f17378d353063df5df8c40da15d767ff2d208fcc
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-10-31 09:37:33 +0100

    exta

--- NASCAR/app.py
+++ NASCAR/app.py
@@ -1,5 +1,8 @@
 import sys
 import os
+from getinfo import get_info
+from getinfo import get_info_from_name
+from getinfo import get_info_best
 
 restart = "N"
 
@@ -9,23 +12,32 @@ def restart_program():
 
 
 def commands():
-    print("to get the current standings Use the command /Standings ")
+    print("for help do /help or /?")
     print("to close the app /exit ")
 
     command = input("Type Command here: ")
 
-    if command == "/Standings" or command == "/exit":
-        if command == "/Standings":
-            print("The current NASCAR Standings are")
+    if command == "/standings" or command == "/standings_name" or command == "/standings_best" or command == "/exit" or command == "/help" or command == "/?":
+        if command == "/standings":
+            get_info()
             commands()
         elif command == "/exit":
             print("program Offline")
+        elif command == "/standings_name":
+            searchname = input("Input driver name here: ")
+            get_info_from_name(searchname)
+            commands()
+        elif command == "/standings_best":
+            get_info_best()
+            commands()
+        elif command == "/help" or command == "/?":
+            print("to get the current standings Use the command /standings ")
+            print("to get the current standings filtered by name Use the command /standings_name ")
+            print("to get the current standings with highest points Use the command /standings_best ")
+            commands()
     else:
         print("Command not found")
         commands()
 
 print("Welcome to the Unofficial NASCAR info Python script")
 commands()
-
-
-
--- NASCAR/getinfo.py
+++ NASCAR/getinfo.py
@@ -1,8 +1,30 @@
 from sql_client import SQLClient
 sql_client = SQLClient()
 
-current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #` FROM standings;")
+def get_info():
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #` FROM standings;")
 
-current_standings_str = str(current_standings)
+    current_standings_str = str(current_standings)
 
-print("The current NASCAR Standings are" + current_standings_str)
+    print("The current NASCAR Cup Standings are: ")
+    for data in current_standings:
+        print(data)
+
+def get_info_from_name(name):
+    name = str(name)
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #` FROM standings WHERE Driver = '" + name + "';")
+
+    current_standings_str = str(current_standings)
+
+    print("The current NASCAR Cup Standings are: ")
+    for data in current_standings:
+        print(data)
+
+def get_info_best():
+    current_standings = sql_client.fetch_all("SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE `Points` = (SELECT MAX(`Points`) FROM standings);")
+
+    current_standings_str = str(current_standings)
+
+    print("The current NASCAR Cup Standings are: ")
+    for data in current_standings:
+        print(data)
