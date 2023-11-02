e32ae94386c335d7412dc36cd9b5f3ce21b6a527
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-11-02 11:38:30 +0100

    error catch

--- getinfo.py
+++ getinfo.py
@@ -1,3 +1,5 @@
+import mysql
+
 from sql_client import SQLClient
 
 sql_client = SQLClient()
@@ -28,15 +30,19 @@ def get_info():
 
 def get_info_from_name(name):
     name = str(name)
-    current_standings = sql_client.fetch_all(
-        "SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Driver = '" + name + "'';")
+    try:
+        current_standings = sql_client.fetch_all(
+            "SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Driver = '" + name + "'';")
 
-    current_standings_str = str(current_standings)
+        current_standings_str = str(current_standings)
 
-    print("The current NASCAR Cup Standings are: ")
-    green_print('DATA FOUND!')
-    for data in current_standings:
-        print(data)
+        print("The current NASCAR Cup Standings are: ")
+        green_print('DATA FOUND!')
+        for data in current_standings:
+            print(data)
+    except mysql.connector.errors.ProgrammingError as mysqlerror:
+        error_text(f"MySQL Error (Code {mysqlerror.errno}): {mysqlerror.msg}")
+
 
 
 def get_info_best():
@@ -51,62 +57,68 @@ def get_info_best():
 
 
 def get_info_driver_manufacturer():
-    manu = str(sql_client.fetch_all("SELECT DISTINCT Make FROM standings"))
-    print('The Manufacturers are' + manu)
-    name = str(input("Input Manufacturer name here: "))
-    current_standings = sql_client.fetch_all(
-        "SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Make = '" + name + "';")
+    try:
+        manu = str(sql_client.fetch_all("SELECT DISTINCT Make FROM standings"))
+        print('The Manufacturers are' + manu)
+        name = str(input("Input Manufacturer name here: "))
+        current_standings = sql_client.fetch_all(
+            "SELECT `Rank`, Driver, `Car #`, Points FROM standings WHERE Make = '" + name + "';")
 
-    current_standings_str = str(current_standings)
+        current_standings_str = str(current_standings)
 
-    print("The current NASCAR Cup Standings are: ")
-    green_print('DATA FOUND!')
-    for data in current_standings:
-        print(data)
+        print("The current NASCAR Cup Standings are: ")
+        green_print('DATA FOUND!')
+        for data in current_standings:
+            print(data)
+    except mysql.connector.errors.ProgrammingError as mysqlerror:
+        error_text(f"MySQL Error (Code {mysqlerror.errno}): {mysqlerror.msg}")
 
 
 def add_competitor():
-    MAX_POINTS_data = sql_client.fetch_all(
-        "SELECT DISTINCT Points FROM standings WHERE `Points` = (SELECT MAX(`Points`) FROM standings);")
-    if MAX_POINTS_data:
-        MAX_POINTS = MAX_POINTS_data[0].get('Points', 0)
-    else:
-        print("No data found")
-    # print(str(MAX_POINTS))
-    rank = int(input("Input standings rank here: "))
-    sts = int(input("Input Starts here: "))
-    driver = str(input("Input DRIVER name here: "))
-    car = int(input("Input Car number here: "))
-    make = str(input("Input Manufacturer name here: "))
-    points = int(input("Input Points here: "))
-    po_pts = int(input("Input Playoff Points here: "))
-    p = int(input("Input Poles here: "))
-    win = int(input("Input wins here: "))
-    t5 = int(input("Input number of Top 5 here: "))
-    t10 = int(input("Input number of Top 10 name here: "))
-    stg_win = int(input("Input stage Wins here: "))
+    try:
+        MAX_POINTS_data = sql_client.fetch_all(
+            "SELECT DISTINCT Points FROM standings WHERE `Points` = (SELECT MAX(`Points`) FROM standings);")
+        if MAX_POINTS_data:
+            MAX_POINTS = MAX_POINTS_data[0].get('Points', 0)
+        else:
+            print("No data found")
+        # print(str(MAX_POINTS))
+        rank = int(input("Input standings rank here: "))
+        sts = int(input("Input Starts here: "))
+        driver = str(input("Input DRIVER name here: "))
+        car = int(input("Input Car number here: "))
+        make = str(input("Input Manufacturer name here: "))
+        points = int(input("Input Points here: "))
+        po_pts = int(input("Input Playoff Points here: "))
+        p = int(input("Input Poles here: "))
+        win = int(input("Input wins here: "))
+        t5 = int(input("Input number of Top 5 here: "))
+        t10 = int(input("Input number of Top 10 name here: "))
+        stg_win = int(input("Input stage Wins here: "))
 
-    bhnd = points - MAX_POINTS
+        bhnd = points - MAX_POINTS
 
-    confirm = input(f'DO you want to add the competitor standing: \n'
-                    f'Rank: {rank}, STS: {sts}, Driver: {driver}, Car: {car}, Make: {make}, Points: {points}, BHND: {bhnd}, PO_PTS: {po_pts}, P: {p}, Win: {win}, T5: {t5}, T10: {t10}, STG_Win: {stg_win}\n'
-                    f'Y/N: ')
+        confirm = input(f'DO you want to add the competitor standing: \n'
+                        f'Rank: {rank}, STS: {sts}, Driver: {driver}, Car: {car}, Make: {make}, Points: {points}, BHND: {bhnd}, PO_PTS: {po_pts}, P: {p}, Win: {win}, T5: {t5}, T10: {t10}, STG_Win: {stg_win}\n'
+                        f'Y/N: ')
 
-    if confirm == 'Y' or confirm == 'y':
-        keys = (
-            "`Rank`", "`STS`", "`Driver`", "`Car #`", "`Make`", "`Points`", "`BHND`", "`PO PTS`", "`P`",
-            "`Win`", "`T5`", "`T10`",
-            "`STG Win`")
-        values = (rank, sts, driver, car, make, points, bhnd, po_pts, p, win, t5, t10, stg_win)
-        table = "standings"
+        if confirm == 'Y' or confirm == 'y':
+            keys = (
+                "`Rank`", "`STS`", "`Driver`", "`Car #`", "`Make`", "`Points`", "`BHND`", "`PO PTS`", "`P`",
+                "`Win`", "`T5`", "`T10`",
+                "`STG Win`")
+            values = (rank, sts, driver, car, make, points, bhnd, po_pts, p, win, t5, t10, stg_win)
+            table = "standings"
 
-        # Use the insert function to insert the data
-        sql_client.insert(keys, values, table)
-        green_print('insert success')
-    elif confirm == 'N' or confirm == 'n':
-        print('Competitor standing add Canceled')
-    else:
-        print('invalid Input')
+            # Use the insert function to insert the data
+            sql_client.insert(keys, values, table)
+            green_print('insert success')
+        elif confirm == 'N' or confirm == 'n':
+            print('Competitor standing add Canceled')
+        else:
+            print('invalid Input')
+    except ValueError as e:
+        error_text(f"Invalid value: {e}")
 
 
 def delete_stand():
